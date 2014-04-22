fb9a33df602e7ed09c2334a0e0a8df63010125d1
dingding <352926@qq.com>
2014-04-22 20:38:39 +0800

    index.php

--- index.php
+++ index.php
@@ -4,4 +4,10 @@
  * Date: 14-4-22
  * Time: 18:58
  */
- 
+
+define('__ROOT__', dirname(__FILE__) . '/'); #站点实际总目录
+define('__CURRENT__', __ROOT__); #站点入口文件所在目录
+define('__SYSTEM__', __ROOT__ . '/system/');
+
+require_once __SYSTEM__ . 'core/ddframe.php';
+
